b001c36544eb07927886884db29fd2b4a685c785
Võ Phước Thạnh <92651849+vophuocthanh@users.noreply.github.com>
2024-02-24 23:33:07 +0700

    feat(BE): add BE

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { serve } from '@hono/node-server';
 import { logger } from 'hono/logger';
 import { router as authRouter } from './modules/auth/auth.controller';
 // import { router as orgsRouter } from './modules/orgs/orgs.controller';
-// import { router as usersRouter } from './modules/users/users.controller';
+import { router as usersRouter } from './modules/users/users.controller';
 // import { router as channelsRouter } from './modules/channels/channels.controller';
 // import { router as categoryRouter } from './modules/category/category.controller';
 import { auth } from './middlewares/auth';
@@ -23,7 +23,7 @@ app.use(
 app.route('/', authRouter);
 
 // app.all('*', auth).route('/orgs', orgsRouter);
-// app.all('*', auth).route('/users', usersRouter);
+app.all('*', auth).route('/users', usersRouter);
 // app.all('*', auth).route('/channels', channelsRouter);
 // app.all('*', auth).route('/category', categoryRouter);
 
