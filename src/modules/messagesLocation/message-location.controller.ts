c3136a21118baf87c44209fc89a8dc6c84f52901
Võ Phước Thạnh <92651849+vophuocthanh@users.noreply.github.com>
2024-05-17 23:18:55 +0700

    feat(BE): add api message location

--- src/modules/messagesLocation/message-location.controller.ts
+++ src/modules/messagesLocation/message-location.controller.ts
@@ -4,31 +4,14 @@ import { zValidator } from '@hono/zod-validator';
 import { Hono } from 'hono';
 import { auth } from '@/middlewares/auth';
 import { checkRole } from '@/middlewares/role';
-import { MessageService } from '@/modules/messages/message.service';
+import { MessageLocationService } from '@/modules/messagesLocation/message.-location.service';
 
 export const router = new Hono();
 
 router
-  .get('/:tourId', auth, async (c) => {
-    const tourId = c.req.param('tourId');
-    const messages = await MessageService.getAll(tourId);
-    return c.json({
-      data: messages,
-    });
-  })
-  .post('/:tourId/message', async (c) => {
-    const user = c.get('user');
-    const tourId = c.req.param('tourId');
-    const createMessageDto = await c.req.json();
-    const message = await MessageService.create(user, tourId, createMessageDto);
-    return c.json({
-      data: message,
-      status: 201,
-    });
-  })
   .get('/:locationId', auth, async (c) => {
     const locationId = c.req.param('locationId');
-    const messages = await MessageService.getAllLocation(locationId);
+    const messages = await MessageLocationService.getAllLocation(locationId);
     return c.json({
       data: messages,
     });
@@ -37,7 +20,7 @@ router
     const user = c.get('user');
     const locationId = c.req.param('locationId');
     const createMessageLocationDto = await c.req.json();
-    const message = await MessageService.createLocation(
+    const message = await MessageLocationService.createLocation(
       user,
       locationId,
       createMessageLocationDto
@@ -49,7 +32,7 @@ router
   })
   .delete('/:messageId', auth, async (c) => {
     const messageId = c.req.param('messageId');
-    await MessageService.delete(messageId);
+    await MessageLocationService.delete(messageId);
     return c.json({
       status: 200,
       message: 'Message deleted successfully',
--- src/modules/messagesLocation/message.-location.service.ts
+++ src/modules/messagesLocation/message.-location.service.ts
@@ -2,39 +2,7 @@ import { db } from '@/lib/db';
 import { Prisma, User } from '@prisma/client';
 import { BadRequestException } from '@/utils/exceptions';
 
-export const MessageService = {
-  getAll: async (tourId: string) => {
-    const message = await db.message.findMany({
-      where: {
-        tour: {
-          id: tourId,
-        },
-      },
-    });
-    return {
-      data: message,
-    };
-  },
-  create: async (
-    user: User,
-    tourId: string,
-    createMessageDto: Prisma.MessageCreateInput
-  ) => {
-    const userId = user ? user.id : null;
-
-    const message = await db.message.create({
-      data: {
-        userID: userId,
-        tour: {
-          connect: {
-            id: tourId,
-          },
-        },
-        ...createMessageDto,
-      },
-    });
-    return message;
-  },
+export const MessageLocationService = {
   getAllLocation: async (locationId: string) => {
     const message = await db.messageLocation.findMany({
       where: {
